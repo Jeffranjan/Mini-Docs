3c18a7abea453dc7c81abe136a40ad3d7e3cc4ad
Ranjan Gupta <Ranjanguptajeff@gmail.com>
2023-12-18 20:00:48 +0530

    Cleaned the code

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 import Card from "./Card";
 
 function Foreground() {
@@ -23,7 +23,6 @@ function Foreground() {
       tag: { isOpen: false, tagTitle: "Upload", tagColor: "green" },
     },
   ];
-  useState();
   return (
     <div>
       <div
@@ -31,7 +30,7 @@ function Foreground() {
         className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5"
       >
         {data.map((item, index) => (
-          <Card data={item} reference={ref} />
+          <Card key={index} data={item} reference={ref} />
         ))}
       </div>
     </div>
